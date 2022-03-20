120d158555da9fb58d4a8f87d770596a61558ec6
Rhys <88878657+Rhys-Davie5@users.noreply.github.com>
2022-03-20 08:41:43 +0000

    Almost there

--- excerise4.py
+++ excerise4.py
@@ -1,18 +1,28 @@
 import random
+
+count = 0
 def userguess():
+    global count
+    count=count+1
+
     useritem = ["dog", "cat", "turtle", "snake", "hamster", "lion", "wolf", "monkey", "bird", "dinosaur", "dragon"]
     userboard1 = random.randint(0, 10)
     userboard2 = random.randint(0, 10)
-    print(userboard1)
-    print(userboard2)
 
     userguess100 = int(input("Please enter guess 1: "))
     userguess200 = int(input("Please enter guess 2: "))
 
     if userboard1 == userguess100 and userboard2 == userguess200:
-        print("Both of them are a match, the first pair was", userboard1, "and the second pair was", userboard2)
+        print("Both of them are a match, the first pair was a", userboard1, "and the second pair was", userboard2)
     else:
-        print("Sorry that was not a match, the first pair was", userboard1, "and the second pair was", userboard2)
+        print("Sorry that was not a match, the first pair was a", userboard1, "and the second pair was", userboard2)
+
+    print("you've had", count, "turn(s) so far")
+    print()
+
+    repeat=userguess()
+    print(repeat)
+
 # -----------------------------------------------------------------------------------------------------------------------
 
 print("Welcome to the pairs game! There are 6 pairs to find")
@@ -22,3 +32,4 @@ print(userboard)
 print("???????????")
 
 userguess()
+
