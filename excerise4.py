dfd6cc20f0ce19476382fbf41d98d4e8d0c5f6eb
Rhys <88878657+Rhys-Davie5@users.noreply.github.com>
2022-03-20 01:33:12 +0000

    Random Changes

--- excerise4.py
+++ excerise4.py
@@ -1,39 +1,23 @@
 import random
+def userguess():
+    useritem = ["dog", "cat", "turtle", "snake", "hamster", "lion", "wolf", "monkey", "bird", "dinosaur", "dragon"]
+    userboard1 = random.randint(1, 10)
+    userboard2 = random.randint(1, 10)
+    print(userboard1)
+    print(userboard2)
 
-def userGuess():
-    userGuess1 = input("Please enter your first guess: ")
-    userGuess2 = input("Please enter your second guess: ")
+    userguess100 = input("Please enter guess 1: ")
+    userguess200 = input("Please enter guess 2: ")
 
-    while userGuess1 == userBoard2:
-        print("its a match")
-    print()
+    if userboard1 == userguess100 and userboard2 == userguess200:
+        print("Both of them are a match, the first pair was", userboard1, "and the second pair was", userboard2)
 
-    while userGuess2 == userBoard3:
-        print("its a match")
-    print("Both of them are a match, the first pair was",userBoard2, "and the second pair was", userBoard3)
-
-    if userGuess1 and userGuess2 != userBoard2 and userBoard3:
-        print("Not a match, the first pair was",userBoard2, "and the second pair was", userBoard3)
-
-#-----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 
 print("Welcome to the pairs game! There are 6 pairs to find")
 print("Here is the board")
-
-userBoard = "0 1 2 3 4 5 6 7 8 9 10"
-print(userBoard)
+userboard = "0 1 2 3 4 5 6 7 8 9 10"
+print(userboard)
 print("???????????")
 
-userBoard2 = random.randint(0, 10)
-userBoard3 = random.randint(0, 10)
-
-print(userBoard2)
-print(userBoard3)
-
-userGuess()
-
-
-
-
-
-
+userguess()
