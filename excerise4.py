07732962d3f7fd8807a50bd4299a9ebaf51b3dff
Rhys <88878657+Rhys-Davie5@users.noreply.github.com>
2022-03-21 09:12:57 +0000

    got an idea

--- excerise4.py
+++ excerise4.py
@@ -3,8 +3,8 @@ import random
 # ----------------------------------------------------------------------------------------------------------------------
 turns = 0
 SetPairs = 6
-Pairs = 0
-
+#Pairs = 0
+PairsN = 1
 
 def userguess():
     userboard1 = random.randint(0, 10)
@@ -13,6 +13,8 @@ def userguess():
     print(userboard2)
     #print(userboard1)
     #print(userboard2)
+
+    userTurns = input("How many turns do you want?")
 # ----------------------------------------------------------------------------------------------------------------------
     userguess100 = int(input("Please enter guess 1: "))
     while userguess100 > 10:
@@ -25,33 +27,47 @@ def userguess():
     userguess200 = int(input("Please enter guess 2: "))
     while userguess200 > 10:
         print("that number is too big, try a smaller number")
-        userguess100 = int(input("Please enter guess 2: "))
+        userguess200 = int(input("Please enter guess 2: "))
     while userguess200 < 0:
         print("that number is too small, try a bigger number")
-        userguess100 = int(input("Please enter guess 2: "))
+        userguess200 = int(input("Please enter guess 2: "))
 
     useritem = ["dog", "cat", "turtle", "snake", "hamster", "lion", "wolf", "monkey", "bird", "dinosaur", "dragon"]
-    useritem2 = (useritem[userguess100]) # guess 1 animal
-    useritem3 = (useritem[userguess200]) # guess 2 animal
+    useritem2 = (useritem[userguess100]) # guess an animal for guess 1
+    useritem3 = (useritem[userguess200]) # guess an animal for guess 1
+
+    dog = 1
+    cat = 1
+    turtle = 1
+    snake = 1
+    hamster = 1
+    lion = 1
+    wolf = 1
+    monkey = 1
+    bird = 1
+    dinosaur = 1
+    dragon = 1
 
 # ----------------------------------------------------------------------------------------------------------------------
     global turns  # count how many times ive looped
-    global Pairs
+  #  global Pairs
     global SetPairs
+    global PairsN
     turns = turns + 1  # adds a 1
-    Pairs = Pairs + 1 # adds a 1
+   # Pairs = Pairs + 1 # adds a 1
+    PairsN = PairsN = 0
     SetPairs = SetPairs
 # ----------------------------------------------------------------------------------------------------------------------
 
-    if Pairs == 6:
-        print("well done, you got them all. you did it in", turns, "turns")
-        print("you have succesfully obtained", Pairs, "pairs, congratulations!")
-        exit()
+   # if Pairs == 6:
+       # print("well done, you got them all. you did it in", turns, "turns")
+       # print("you have succesfully obtained", Pairs, "pairs, congratulations!")
+       # exit()
 
-    if turns == 6:
-        print("well done, you got them all. you did it in", turns, "turns")
-        print("you have succesfully obtained", Pairs, "pairs, congratulations!")
-        exit()
+    #if turns == 6:
+        #print("well done, you got them all. you did it in", turns, "turns")
+        #print("you have succesfully obtained", Pairs, "pairs, congratulations!")
+       # exit()
 
     if userboard1 == userguess100 and userboard2 == userguess200:
         print("Both of them are a match, the first pair", userguess100, "was a", useritem2, "and the second pair",
@@ -59,10 +75,13 @@ def userguess():
         print("you now have", Pairs, "pair(s)")
         print("you have done", turns, "turn(s)")
 
-    elif userboard1 != userguess100 and userboard2 != userguess200:
+
+    else:
         print("Both of them are not a match, the first pair", userguess100, "was a", useritem2, "and the second pair", userguess200, "was a", useritem3)
-        print("you now have", Pairs-1, "pair(s)")
+        print("you now have", PairsN, "pair(s)")
         print("you have", turns, "turn(s) so far")
+
+
 
 # ----------------------------------------------------------------------------------------------------------------------
     print()
