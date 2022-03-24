0faed680b1fb397aa2b7ccc6195ec4e9f0e15648
Rhys <88878657+Rhys-Davie5@users.noreply.github.com>
2022-03-24 16:10:06 +0000

    omg

--- gotsomething.py
+++ gotsomething.py
@@ -1,5 +1,5 @@
 import random
-
+totalno = 10
 userboard1 = random.randint(0, 10)
 userboard2 = random.randint(0, 10)
 print(userboard1)
@@ -9,32 +9,41 @@ userguess100 = int(input("Please enter guess 1: "))
 while userguess100 > 10:
     print("that number is too big, try a smaller number")
     userguess100 = int(input("Please enter guess 1: "))
-    while userguess100 < 0:
-        print("that number is too small, try a bigger number")
-        userguess100 = int(input("Please enter guess 1: "))
+while userguess100 < 0:
+    print("that number is too small, try a bigger number")
+    userguess100 = int(input("Please enter guess 1: "))
 
+userguess200 = int(input("Please enter guess 2: "))
+while userguess200 > 10:
+    print("that number is too big, try a smaller number")
     userguess200 = int(input("Please enter guess 2: "))
-    while userguess200 > 10:
-        print("that number is too big, try a smaller number")
-        userguess200 = int(input("Please enter guess 2: "))
-    while userguess200 < 0:
-        print("that number is too small, try a bigger number")
-        userguess200 = int(input("Please enter guess 2: "))
+while userguess200 < 0:
+    print("that number is too small, try a bigger number")
+    userguess200 = int(input("Please enter guess 2: "))
 
 useritem = ["dog", "cat", "turtle", "snake", "hamster", "lion", "wolf", "monkey", "bird", "dinosaur", "dragon"]
 useritems = ["dog", "cat", "turtle", "snake", "hamster", "lion", "wolf", "monkey", "bird", "dinosaur", "dragon"]
-for counter in range(0, 10):
-    print(useritem[counter])
+#for counter in range(0, 10):
+ #   print(useritem[counter])
 
-useritem2 = (useritem[userguess100]) # guess an animal for guess 1
-useritem3 = (useritems[userguess200]) # guess an animal for guess 1
+useritem2 = (useritem[userguess100]) # get animal for guess 1
+useritem3 = (useritems[userguess200]) # get an animal for guess 1
 
-if "dog" in useritem and useritems:
+print(useritem2, useritem3)
+
+
+if useritem2 == useritem3:
     print("Thats a match!")
+    #pop
+    totalno=totalno-1
+
+else:
+    print("Not a match!")
 
 
 
-if "cat" in useritem and useritems:
-    print("Thats a match")
+
+#if "cat" in useritem2 and useritem3:
+    #print("Thats a match")
 
 
--- test123.py
+++
@@ -1,5 +0,0 @@
-myValue = 10
-while myValue < 10:
-    print("Hello", myValue)
-
-print("end of program")
