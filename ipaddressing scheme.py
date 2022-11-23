7a191ddf8415487acef10a9bb19a032dbc177cb2
Rhys <88878657+Rhys-Davie5@users.noreply.github.com>
2022-11-23 15:07:24 +0000

    ipaddressing

---
+++ ipaddressing scheme.py
@@ -0,0 +1,7 @@
+Number_of_subnets = [1,   2,   4,  8,  16, 32, 64, 128, 256]
+Number_of_hosts =   [256, 128, 64, 32, 16, 8,  4,  2,   1]
+Number_of_subnet_mask = ["/24","/25","/26","/27","/28","/29",]
+NetworkID = input("Please enter the Network address ")
+
+
+
